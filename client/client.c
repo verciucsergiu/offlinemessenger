f76ca6d244711be827bde1b8bf7509cc399063ce
Verciuc Sergiu <v_sergiu2009@yahoo.com>
2018-01-05 01:05:52 +0200

    Client adjustments.

--- client/client.c
+++ client/client.c
@@ -9,17 +9,35 @@
 #include <string.h>
 #include <ncurses.h>
 
+#include "client_structures.h"
+
 extern int errno;
 
 #define PORT 3001
+
+char message[256];
+int msg_index = 0;
+
+int connected = 1;
+
+messages_collection messages;
+
+int send_message(int descriptor);
+void append_message(char message[256]);
+void display_messages();
+void refresh_message();
 
 int main (int argc, char *argv[])
 {
   int sd;
   struct sockaddr_in server;
-  char buf[10];
+  messages.count = 0;
 
-  /* cream socketul */
+  initscr();
+  clear();
+  noecho();
+  raw();
+
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
   {
     perror ("Eroare la socket.\n");
@@ -32,72 +50,99 @@ int main (int argc, char *argv[])
 
   server.sin_port = htons (PORT);
 
-  // if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
-  // {
-  //   perror ("Eroare la connect.\n");
-  //   return errno;
-  // }
-  initscr();
-  clear();
-  noecho();
-  cbreak();
+  if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
+  {
+    perror ("Eroare la connect.\n");
+    return errno;
+  }
+  
   if(fork()) 
   {
-    char msg[256];
-    int i=0;
-    while(1)
+    while(connected)
     {
       int ch;
       ch = getch();
-      printw("%c", ch);
-      // if (ch != '\0')
-      // {
-      //   printw("%s", msg);
-      // } 
-      // else if (ch != '\b')
-      // {
-      //   msg[i++] = ch;
-      // }
-      // else 
-      // {
-      //   msg[i] = '\0';
-      //   i--;
-      // }
-      refresh();
+      if (ch == '\n')
+      {
+        message[msg_index] = '\0';
+        send_message(sd);
+      } 
+      else if (ch == 127) // delete because backspace does not exists :(
+      {
+        if(msg_index > 0)
+        {
+          msg_index--;
+          message[msg_index] = '\0';
+          display_messages();
+        }
+      }
+      else 
+      {
+        printw("%c", ch);      
+        message[msg_index++] = ch;
+        message[msg_index] = '\0';
+      }
     }
-    // char msg[256]; // stringul trimis cater server.
-    // while(1)
-    // {
-    //   /* citirea mesajului */
-    //   printf ("$:");
-    //   fflush (stdout);
-    //   fgets(&msg, 256, stdin);
-    //   /* trimiterea mesajului la server */
-
-    //   if (write (sd,&msg, sizeof(msg)) <= 0)
-    //   {
-    //     perror ("Eroare la write() spre server.\n");
-    //     return errno;
-    //   }
-    // }
   }
   else
   {
-    char msg2[256]; // stringul primit de la server.
-    // while(1) 
-    // {
-    //   /* citirea raspunsului dat de server 
-    //     (apel blocant pina cind serverul raspunde) */
-    //   if (read (sd, &msg2, 256) < 0)
-    //   {
-    //     perror ("Eroare la read() de la server.\n");
-    //     return errno;
-    //   }
-    //   /* afisam mesajul primit */
-    //   printf ("$: %s", msg2);
-    //   fflush (stdout);
-    // }
+    char server_msg[256];
+    while(connected) 
+    {
+      if (read (sd, &server_msg, 256) < 0)
+      {
+        perror ("Eroare la read() de la server.\n");
+        return errno;
+      }
+      /* afisam mesajul primit */
+      append_message(server_msg);
+      display_messages();
+      refresh();
+    }
   }
   /* inchidem conexiunea, am terminat */
+  endwin();
   close (sd);
+}
+
+
+int send_message(int descriptor)
+{
+  if (strcmp(message, "/close") == 0)
+  {
+    connected = 0;
+  } 
+  else if (write (descriptor,&message, sizeof(message)) <= 0)
+  {
+    perror ("Eroare la write() spre server.\n");
+    return errno;
+  }
+  refresh_message();
+}
+
+void append_message(char message[256])
+{
+  strcpy(messages.list[messages.count++], message);
+}
+
+void display_input(char message[256])
+{
+  printw("=> you were typing: %s", message);
+}
+
+void display_messages()
+{
+  clear();
+  int index;
+  for(index = 0; index < messages.count; index ++)
+  {
+    printw("%s\n", messages.list[index]);
+  }
+  refresh();
+}
+
+void refresh_message()
+{
+  msg_index = 0;
+  message[msg_index] = '\0';
 }
---
+++ client/client_structures.h
@@ -0,0 +1,4 @@
+typedef struct messages_collection {
+    int count;
+    char list[10][256];
+}messages_collection;
